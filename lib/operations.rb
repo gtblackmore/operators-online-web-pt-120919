c915000be2bc972c7dc28a97ba4b0ee108bf7bc2
George Blackmore <gtblackmore@gmail.com>
2019-12-11 03:32:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,10 @@
 def unsafe?(speed)
-  speed > 60 || speed < 40
+  if speed > 60
+    p true
+  elsif speed < 40
+    p true
+  else
+    p false
 end
 
 
@@ -8,8 +13,6 @@ def not_safe?(speed)
 	speed > 60 ? true : false
 	speed < 60 ? true : false
 	speed >= 40 || speed <= 60 ? false : true
-	
-	
 end
 	
 
